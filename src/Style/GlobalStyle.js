e86fcabce59ab1d1b3c392c3f9ecf879a705a565
Mandy Schwarze <mandy2301@me.com>
2021-05-05 23:37:20 +0200

    Add Global Style

---
+++ src/Style/GlobalStyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components/macro'
+
+const GlobalStyle = createGlobalStyle`
+    html {
+        margin: 0;
+        box-sizing: border-box;
+        background: #333;
+        color: whitesmoke;
+        font-family: 'Open Sans';
+    }
+`
+export default GlobalStyle
