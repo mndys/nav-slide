d7214b58e36a7cc8cc9d80fa72a3ad305cab86f2
Mandy Schwarze <mandy2301@me.com>
2021-05-06 19:18:00 +0200

    Add css variables

--- src/Style/GlobalStyle.js
+++ src/Style/GlobalStyle.js
@@ -3,18 +3,21 @@ import { createGlobalStyle } from 'styled-components/macro'
 const GlobalStyle = createGlobalStyle`
     html {
         box-sizing: border-box;
+        --size-xs: .1rem;
+        --size-s: .3rem;
+        --size-m: .5rem;
+        --size-l: 1rem;
+        --size-xl: 1.5rem;
+        --color-bg: #222;
+        --color-primary: #000;
+        --color-text: #e3e3e3;
     }
 
     body {
         margin: 0;
-        background: #333;
-        color: whitesmoke;
+        background: var(--color-bg);
+        color: var(--color-text);
         font-family: 'Open Sans';
-    }
-
-    li {
-        margin: .5rem 1rem;
-        padding: .3rem .7rem;
     }
 `
 export default GlobalStyle
